52db6babcbc7e5e24ddb822b041198ae8808289a
Ikenna Onugha <i.onugha@alustudent.com>
2025-06-26 23:42:51 +0200

    pep8 adjusted

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -4,15 +4,16 @@ number = random.randint(-10000, 10000)
 string_number = str(number)
 last_digit = string_number[-1]
 int_last_digit = int(last_digit)
-if number < 0 :
+if number < 0:
     int_last_digit *= -1
 
 def checker(num):
-    if num > 5 :
+    
+    if num > 5:
         return "and is greater than 5"
     elif num == 0:
         return "and is 0"
-    elif num < 6 and num != 0 :
+    elif num < 6 and num != 0:
         return "and is less than 6 and not 0"
 
 message = checker(int_last_digit)
