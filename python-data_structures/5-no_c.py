3f7b1cdbd865141baf198d3f70a359bc8e8ac673
Ikenna Onugha <i.onugha@alustudent.com>
2025-06-27 22:55:47 +0200

    changed whole logic

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in my_string:
-        if i not in ("c", "C"):
-            print(i, end='')
+    new_string = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
+    return new_string
