d4cd495bf2ca36886fd320de8856319139c90967
Ikenna Onugha <i.onugha@alustudent.com>
2025-06-27 09:45:31 -0700

    learned argv

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    
+    if len(sys.argv) == 0:
+        print("{} arguments.".format(len(sys.argv)))
+    elif len(sys.argv) == 1:
+        print("1 argument:")
+        print("1: {}".format(sys.arg[0]))
+    else:
+        print("{} arguments:".format(len(sys.argv)))
+        for i, arg in enumerate(sys.arg[1:]):
+            print("{}: {}".format(i + 1, arg))
