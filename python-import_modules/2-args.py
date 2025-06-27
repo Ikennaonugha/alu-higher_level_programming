8c754fd08a5ac5710e3a8d012edaf5b328d8896c
Ikenna Onugha <i.onugha@alustudent.com>
2025-06-27 18:47:25 +0200

    argv

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
         print("{} arguments.".format(len(sys.argv)))
     elif len(sys.argv) == 1:
         print("1 argument:")
-        print("1: {}".format(sys.arg[0]))
+        print("1: {}".format(sys.argv[0]))
     else:
         print("{} arguments:".format(len(sys.argv)))
-        for i, arg in enumerate(sys.arg[1:]):
+        for i, arg in enumerate(sys.argv[1:]):
             print("{}: {}".format(i + 1, arg))
