d6826dd4651242941f74f71fe6e8711e808fe038
Ikenna Onugha <i.onugha@alustudent.com>
2025-06-27 18:57:50 +0200

    changed logic

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,13 +2,14 @@
 import sys
 
 if __name__ == "__main__":
-    
-    if len(sys.argv) == 0:
-        print("{} arguments.".format(len(sys.argv)))
-    elif len(sys.argv) == 1:
+
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+
+    if len(sys.argv) == 2:
         print("1 argument:")
-        print("1: {}".format(sys.argv[0]))
-    else:
-        print("{} arguments:".format(len(sys.argv)))
+        print("1: {}".format(sys.argv[1]))
+
+    if len(sys.argv) > 2:
         for i, arg in enumerate(sys.argv[1:]):
             print("{}: {}".format(i + 1, arg))
