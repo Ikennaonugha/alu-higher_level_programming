ded784c3cbe25cabb2c91e41989e3791caebcba1
Ikenna Onugha <i.onugha@alustudent.com>
2025-06-27 13:04:58 +0200

    used modern f string

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -2,7 +2,7 @@
 def print_last_digit(number):
 
     if number >= 0:
-        return "{}".format(number % 10)
+        return f"{number % 10}"
     else:
-        return "-{}".format(abs(number) % 10)
+        return f"-{abs(number) % 10}"
     print("\n", end='')
