703e12d300faac4798f34ff4e2ca245bf0eb97f7
Ikenna Onugha <i.onugha@alustudent.com>
2025-06-27 13:03:12 +0200

    changed print to return

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -2,6 +2,7 @@
 def print_last_digit(number):
 
     if number >= 0:
-        print("{}".format(number % 10), end='')
+        return "{}".format(number % 10)
     else:
-        print("-{}".format(abs(number) % 10), end='')
+        return "-{}".format(abs(number) % 10)
+    print("\n", end='')
