d58651c1789052625500bb90bc083c7d132f52ae
Ikenna Onugha <i.onugha@alustudent.com>
2025-06-27 12:58:20 +0200

    adjusted logic

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
 
-    if number <= 0:
+    if number >= 0:
         print("{}".format(number % 10))
     else:
         print("-{}".format(abs(number) % 10))
